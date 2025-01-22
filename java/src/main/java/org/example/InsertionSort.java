14d9a4eb54ea1c55be8ff7e390a18b9e36275859
XiangchaoHou <hou.xian@northeastern.edu>
2025-01-22 16:18:49 -0500

    16.18

--- java/src/main/java/org/example/InsertionSort.java
+++ java/src/main/java/org/example/InsertionSort.java
@@ -3,13 +3,18 @@ package org.example;
 public class InsertionSort {
     //Sorts the array using Insertion Sort.
     public void sort(Comparable[] a) {
-        for (int i = 1; i < a.length; i++) {
+        for (int i = 0; i < a.length; i++) {
             insert(i, a);
         }
     }
 
     private void insert(int i, Comparable[] a) {
         //Inserts the 'Transition element' into its correct position in the sorted portion of the array.
+        for(int j = i; j > 0; j--) {
+            if(a[j].compareTo(a[j-1]) < 0) {
+                swap(a, j, j-1);
+            }
+        }
         //TODO: TO BE IMPLEMENTED
     }
 
